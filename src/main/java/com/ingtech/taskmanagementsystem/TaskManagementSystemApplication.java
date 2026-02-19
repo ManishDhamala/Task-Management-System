cea698173af294429f4fb74a7516b56c546d0988
ManishDhamala <dhamalamanish32@gmail.com>
2026-02-19 14:19:52 +0545

    Initial Commit

---
+++ src/main/java/com/ingtech/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.ingtech.taskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TaskManagementSystemApplication.class, args);
+
+        System.out.println("Hello World !");
+        System.out.println("The program has started");
+
+    }
+
+}
