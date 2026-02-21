d3d1a31a48eb3d7329a09a1e5f0e43791b07096e
ManishDhamala <dhamalamanish32@gmail.com>
2026-02-21 20:43:44 +0545

    test: added unit test for get all tasks service method

--- src/test/java/com/ingtech/taskmanagementsystem/service/TaskServiceImplTest.java
+++ src/test/java/com/ingtech/taskmanagementsystem/service/TaskServiceImplTest.java
@@ -12,10 +12,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,9 +38,13 @@ class TaskServiceImplTest {
 
     private TaskRequestDto requestDto;
 
-    private Task task;
+    private Task task1;
 
-    private TaskResponseDto responseDto;
+    private Task task2;
+
+    private TaskResponseDto responseDto1;
+
+    private TaskResponseDto responseDto2;
 
     @BeforeEach
     void setUp() {
@@ -46,29 +56,44 @@ class TaskServiceImplTest {
         requestDto.setTaskStatus(Status.PENDING);
         requestDto.setDueDate(LocalDate.of(2026, 8, 30));
 
-        task = new Task();
-        task.setId(1L);
-        task.setTitle("Test Task");
-        task.setDescription("Test Description");
-        task.setTaskStatus(Status.PENDING);
-        task.setDueDate(LocalDate.of(2026, 8, 30));
+        task1 = new Task();
+        task1.setId(1L);
+        task1.setTitle("Test Task");
+        task1.setDescription("Test Description");
+        task1.setTaskStatus(Status.PENDING);
+        task1.setDueDate(LocalDate.of(2026, 8, 30));
 
-        responseDto = new TaskResponseDto();
-        responseDto.setId(1L);
-        responseDto.setTitle("Test Task");
-        responseDto.setDescription("Test Description");
-        responseDto.setTaskStatus(Status.PENDING);
-        responseDto.setDueDate(LocalDate.of(2026, 8, 30));
+        task2 = new Task();
+        task2.setId(2L);
+        task2.setTitle("Test Task 2");
+        task2.setDescription("Test Description 2");
+        task2.setTaskStatus(Status.IN_PROGRESS);
+        task2.setDueDate(LocalDate.of(2026, 9, 30));
+
+
+        responseDto1 = new TaskResponseDto();
+        responseDto1.setId(1L);
+        responseDto1.setTitle("Test Task");
+        responseDto1.setDescription("Test Description");
+        responseDto1.setTaskStatus(Status.PENDING);
+        responseDto1.setDueDate(LocalDate.of(2026, 8, 30));
+
+        responseDto2 = new TaskResponseDto();
+        responseDto2.setId(2L);
+        responseDto2.setTitle("Test Task 2");
+        responseDto2.setDescription("Test Description 2");
+        responseDto2.setTaskStatus(Status.IN_PROGRESS);
+        responseDto2.setDueDate(LocalDate.of(2026, 9, 30));
 
     }
 
 
     @Test
-    void createTask_validRequest_returnsTaskResponseDto(){
+    void createTask_validRequest_returnsTaskResponseDto() {
 
-        when(taskMapper.toEntity(requestDto)).thenReturn(task);
-        when(taskRepository.save(task)).thenReturn(task);
-        when(taskMapper.toDto(task)).thenReturn(responseDto);
+        when(taskMapper.toEntity(requestDto)).thenReturn(task1);
+        when(taskRepository.save(task1)).thenReturn(task1);
+        when(taskMapper.toDto(task1)).thenReturn(responseDto1);
 
         // Act
         TaskResponseDto result = taskService.createTask(requestDto);
@@ -83,8 +108,49 @@ class TaskServiceImplTest {
 
         // Verify method calls
         verify(taskMapper, times(1)).toEntity(requestDto);
-        verify(taskRepository,times(1)).save(task);
-        verify(taskMapper, times(1)).toDto(task);
+        verify(taskRepository, times(1)).save(task1);
+        verify(taskMapper, times(1)).toDto(task1);
+
+    }
+
+    @Test
+    void getAllTasks_validPageable_returnsPageOfTaskResponseDto() {
+
+        Pageable pageable = PageRequest.of(0, 5);
+
+        Page<Task> taskPage = new PageImpl<>(List.of(task1, task2), pageable, 2);
+
+        when(taskRepository.findAll(pageable)).thenReturn(taskPage);
+        when(taskMapper.toDto(task1)).thenReturn(responseDto1);
+        when(taskMapper.toDto(task2)).thenReturn(responseDto2);
+
+        //Act
+        Page<TaskResponseDto> result = taskService.getAllTasks(pageable);
+
+        //Assert
+
+        // Pagination validation
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(2, result.getContent().size());
+
+        assertEquals(1L, result.getContent().get(0).getId());
+        assertEquals("Test Task", result.getContent().get(0).getTitle());
+        assertEquals("Test Description", result.getContent().get(0).getDescription());
+        assertEquals(Status.PENDING, result.getContent().get(0).getTaskStatus());
+        assertEquals(LocalDate.of(2026, 8, 30), result.getContent().get(0).getDueDate());
+
+        assertEquals(2L, result.getContent().get(1).getId());
+        assertEquals("Test Task 2", result.getContent().get(1).getTitle());
+        assertEquals("Test Description 2", result.getContent().get(1).getDescription());
+        assertEquals(Status.IN_PROGRESS, result.getContent().get(1).getTaskStatus());
+        assertEquals(LocalDate.of(2026, 9, 30), result.getContent().get(1).getDueDate());
+
+
+        // Verify
+        verify(taskRepository, times(1)).findAll(pageable);
+        verify(taskMapper, times(1)).toDto(task1);
+        verify(taskMapper, times(1)).toDto(task2);
 
     }
 
